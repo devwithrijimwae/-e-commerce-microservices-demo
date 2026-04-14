f289c81aef2f31a109abdcf9244c7a9953cd4fbd
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2026-04-14 11:47:30 +0100

    Feat:Create eCommerce SharedLibrary

---
+++ eCommerceSharedLibary/Interface/IGenericIterface.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.SharedLibary.Response;
+using System.Linq.Expressions;
+namespace eCommerceSharedLibary.Interface
+{
+    public interface IGenericIterface <T> where T : class
+    { 
+        Task<Response>CreateAsync(T entity);
+        Task<Response>Update(T entity);
+        Task<Response>DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> FindByIdAsync(int id);
+        Task<T> GetByAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ eCommerceSharedLibary/Middleware/GlobalException.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace eCommerce.SharedLibary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Deleclare variables
+            string message = "sorry, internal srever error occurred.kindly try again";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+            try
+            {
+                await next(context);
+                //chech if Response is too many request // 429 status code
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many request made.";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+
+                }
+
+                //if response is unauthorized // 401 status code
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access.";
+                    statusCode = (int)StatusCodes.Status401Unauthorized;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+                // If Response is Forbidden // 403 status code
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "Ypu are not allowed/required to access.";
+                    statusCode = StatusCodes.Status403Forbidden;
+                }
+            }
+
+        private async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            //display scary-free message to client
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+
+                Detail = message,
+                Status = statusCode,
+                Title = title
+
+            }),CancellationToken.None);
+
+        }
+    }
+}
