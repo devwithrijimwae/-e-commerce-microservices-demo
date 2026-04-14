0830e95fae088b352c8502874c078f39d424de2c
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2026-04-14 18:08:03 +0100

    Feat:Add JWT Authentication and Shared Service Container

---
+++ eCommerceSharedLibary/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,53 @@
+﻿using eCommerce.SharedLibary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>
+            (this IServiceCollection services, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            //Add Generic Database content
+            services.AddDbContext<TContext>(options => options.UseSqlServer(config
+                .GetConnectionString("eCommerceConnection"), sqlserverOption => sqlserverOption.EnableRetryOnFailure()));
+            //configure serilog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3] {message:lj{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            //Add JWT Authentication Scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            //Use global Exception
+            app.UseMiddleware<GlobalException>();
+
+            // Register middleware to all outs
+            app.UseMiddleware<ListenToOnlyApiGetway>();
+
+
+            return app;
+        }
+
+    }
+}
