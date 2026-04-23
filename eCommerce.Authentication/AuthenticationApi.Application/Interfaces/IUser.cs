08ed1226cdc684cff8464f0f5e0bc783eaa2b660
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2026-04-23 14:53:33 +0100

    feat:create repository for authentication

---
+++ eCommerce.Authentication/AuthenticationApi.Application/Interfaces/IUser.cs
@@ -0,0 +1,13 @@
+﻿using AuthenticationApi.Application.DTOs;
+using eCommerce.SharedLibary.Response;
+
+namespace AuthenticationApi.Application.Interfaces
+{
+    public interface IUser
+    {
+        Task<Response> Register(AppUserDTO appUserDTO);
+        Task<Response> Login(LoginDTO loginDTO);
+
+        Task<GetUserDTO> GetUser(int userId);
+    }
+}
